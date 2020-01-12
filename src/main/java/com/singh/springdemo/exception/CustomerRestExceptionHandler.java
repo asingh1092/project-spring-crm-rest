ef89e068896d46d0e6f43f4caca2eb85b15ca4f9
Amrit Singh <asingh1092@gmail.com>
2020-01-11 21:06:09 -0500

    exception handling logic

---
+++ src/main/java/com/singh/springdemo/exception/CustomerRestExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.singh.springdemo.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException e) {
+		CustomerErrorResponse error = new CustomerErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(e.getMessage());
+		error.setTimeStamp(LocalDateTime.now());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception e) {
+		CustomerErrorResponse error = new CustomerErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(e.getMessage());
+		error.setTimeStamp(LocalDateTime.now());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+}
