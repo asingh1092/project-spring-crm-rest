8d9b8ba4526af0cf1b09497d948628ed54ba98b3
Amrit Singh <asingh1092@gmail.com>
2020-01-11 20:45:29 -0500

    Get api and get all api for customers

---
+++ src/main/java/com/singh/springdemo/controller/CustomerRestController.java
@@ -0,0 +1,32 @@
+package com.singh.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.singh.springdemo.entity.Customer;
+import com.singh.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+	
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		return customerService.getCustomer(customerId);
+	}
+
+}
