caa89eaf882c6b9c4531c6c057dff3bf8da869db
Amrit Singh <asingh1092@gmail.com>
2020-01-11 21:35:27 -0500

    add remaining CRUD operations (POST, PUT, DELETE)

--- src/main/java/com/singh/springdemo/controller/CustomerRestController.java
+++ src/main/java/com/singh/springdemo/controller/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.singh.springdemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +35,27 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer not found with id: " + customerId);
 		}
 		return customer;
+	}
+	
+	// add mapping for POST / customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+		// pass an ID in JSON, set ID to 0
+		// this is a force save of new item instead of an update
+		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public void deleteCustomer(@PathVariable int customerId) {
+		customerService.deleteCustomer(customerId);
 	}
 
 }
